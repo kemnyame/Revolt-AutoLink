0ea2e1f2a73964bf3cf82dabb164cebc0ebbe44e
kemnyame <51462465+kemnyame@users.noreply.github.com>
2026-09-10 10:49:07 +0000

    Add files via upload

--- public/ecosystem.js
+++ public/ecosystem.js
@@ -1,3 +1,4 @@
+function esc(s){return String(s??'').replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/\"/g,'&quot;').replace(/'/g,'&#39;')}
 // Revolt ecosystem expansion
 let cart=JSON.parse(sessionStorage.getItem('revoltCart')||'[]');
 const cartSave=()=>sessionStorage.setItem('revoltCart',JSON.stringify(cart));
